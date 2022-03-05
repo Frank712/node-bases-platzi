47b09e81fd04705cb1f2266d3dbb1f6054bae2a3
Frank712 <franksuriel_7@hotmail.com>
2022-03-04 23:05:39 -0600

    ADD scrapping tool

---
+++ tools/scrapping/index.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  console.log("Lanzamos navegador");
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto("https://es.wikipedia/wiki/Node.js");
+
+  let title1 = await page.evaluate(() => {
+    const h1 = document.querySelector("h1");
+    return h1.innerHTML;
+  });
+
+  console.log(title1);
+  console.log("Cerramos navegador");
+  // browser.close();
+  console.log("Navegador cerrado!");
+})();
